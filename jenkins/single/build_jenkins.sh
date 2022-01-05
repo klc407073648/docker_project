9a0adff23d68818b9222026fd730e311bd911493
klc407073648 <407073648@qq.com>
2022-01-05 19:15:44 +0800

    jenkins和elasticsearch内容更新

---
+++ jenkins/single/build_jenkins.sh
@@ -0,0 +1,15 @@
+#/bin/bash
+build_prj_name=jenkins
+echo "begin to clear environment stibel_${build_prj_name}"
+
+docker stop $(docker ps |grep stibel_${build_prj_name}_0 |awk '{print $1}')
+docker rm $(docker ps -q -f status=exited)
+#docker rmi stibel_${build_prj_name}:v1.0
+
+echo "end to clear environment stibel_${build_prj_name}"
+
+echo "begin to build stibel_${build_prj_name}"
+
+docker-compose up -d
+
+echo "end to build stibel_${build_prj_name}"
